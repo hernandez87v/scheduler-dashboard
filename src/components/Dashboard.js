9393ac4054831554abef6fe08578c2588898241d
hernandez87v <hernandez87v@hotmail.com>
2020-04-23 15:19:06 -0700

    Pulled data from API to render

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -3,27 +3,33 @@ import classnames from 'classnames';
 import Loading from 'components/Loading';
 import Panel from 'components/Panel';
 import axios from 'axios';
+import {
+  getTotalInterviews,
+  getLeastPopularTimeSlot,
+  getMostPopularDay,
+  getInterviewsPerDay,
+} from 'helpers/selectors';
 
 const data = [
   {
     id: 1,
     label: 'Total Interviews',
-    value: 6,
+    getValue: getTotalInterviews,
   },
   {
     id: 2,
     label: 'Least Popular Time Slot',
-    value: '1pm',
+    getValue: getLeastPopularTimeSlot,
   },
   {
     id: 3,
     label: 'Most Popular Day',
-    value: 'Wednesday',
+    getValue: getMostPopularDay,
   },
   {
     id: 4,
     label: 'Interviews Per Day',
-    value: '2.3',
+    getValue: getInterviewsPerDay,
   },
 ];
 
@@ -87,7 +93,7 @@ class Dashboard extends Component {
         <Panel
           key={panel.id}
           label={panel.label}
-          value={panel.value}
+          value={panel.getValue(this.state)}
           onSelect={(event) => this.selectPanel(panel.id)}
         />
       ));
